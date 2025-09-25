3afdedea2b73540f424036bf3fe2e06427791321
PedroLuiz0000 <pllp1@aluno.ifnmg.edu.br>
2025-09-25 19:23:15 +0000

    .

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-import express from "express";      // Requisição do pacote do express
+import express from "express";  
+import pkg from "pg";
+import dotenv from "dotenv";  
+dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres  // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
 app.get("/", (req, res) => {        // Cria endpoint na rota da raiz do projeto
+  
   console.log("Rota GET / solicitada");
   res.json({
 		message: "API para limpar o computador",      // Substitua pelo conteúdo da sua API
     author: "Pedro Luiz Lopes Pereira",    // Substitua pelo seu nome
+  statusBD: dbStatus
   });
 });
 
